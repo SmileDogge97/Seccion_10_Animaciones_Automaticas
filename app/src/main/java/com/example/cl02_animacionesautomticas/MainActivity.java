0eee1c47872684b181c171e3e0961ff54e8c4c4c
SmileDogge97 <Ing_JosFco_JarMtz_SD@outlook.es>
2022-11-29 10:44:53 -0600

    se incorporó la animación automática

--- app/src/main/java/com/example/cl02_animacionesautomticas/MainActivity.java
+++ app/src/main/java/com/example/cl02_animacionesautomticas/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.cl02_animacionesautomticas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvInfo = findViewById(R.id.textViewInformacion);
+    }
+
+    public void cambiarVisibilidad(View view) {
+        Log.i("MainActivity/cambiarVisibilidad", "si entró");
+        if (tvInfo.getVisibility() == View.VISIBLE){
+            Log.i("MainActivity/cambiarVisibilidad", "estaba visible");
+            tvInfo.setVisibility(View.GONE);
+        } else {
+            Log.i("MainActivity/cambiarVisibilidad", "no estaba visible");
+            tvInfo.setVisibility(View.VISIBLE);
+        }
     }
 }
